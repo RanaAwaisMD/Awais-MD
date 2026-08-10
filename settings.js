8c6f0e039b0f6aa69ee0b450cb165d8d9b6b8b15
RanaAwaisMD <ra36197714@gmail.com>
2026-08-10 09:53:20 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
     giphyApiKey: process.env.GIPHY_API_KEY || 'dc6zaTOxFJmzC',
-    ownerNumber: process.env.OWNER_NUMBER || '923039507788',
-    botName: '𝗟𝗘𝗚𝗘𝗡𝗗 𝗠𝗗 𝗕𝗢𝗧',
-    ownerName: '𝗟𝗘𝗚𝗘𝗡𝗗 𝗛𝗔𝗦𝗦𝗔𝗡'
+    ownerNumber: process.env.OWNER_NUMBER || '923126184997',
+    botName: '𝘼𝙬𝙖𝙞𝙨 𝙈𝘿 𝘽𝙤𝙩',
+    ownerName: '𝙍𝙖𝙣𝙖 𝘼𝙬𝙖𝙞𝙨 𝙈𝘿'
 };
